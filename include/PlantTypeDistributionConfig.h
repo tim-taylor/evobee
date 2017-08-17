b6dbaaeb3298b5c5a3278ed71063331256df045c
Tim Taylor <tim@tim-taylor.com>
2017-08-17 15:22:16 +1000

    Minor changes to comments

--- include/PlantTypeDistributionConfig.h
+++ include/PlantTypeDistributionConfig.h
@@ -23,19 +23,19 @@ struct PlantTypeDistributionConfig {
     {}
 
     unsigned int id;                ///< Each PTDC has a unique id assigned to it
-                                    ///<   (assignment is done in ModelParams::addPTDC)
+                                    ///<   (assignment is done in ModelParams::addPlantTypeDistributionConfig)
 
     std::string species;            ///< Name of the plant species
     iPos    areaTopLeft;            ///< Top-left corner of initial distribution area
     iPos    areaBottomRight;        ///< Bottom-right corner of initial distribution area
     float   density;                ///< Density of initial distribution (wrt specified distrib area)
     bool    refuge;                 ///< Is this area a refuge for this plant species?
-    float   refugeAlienInflowProb;  ///< If this is a refuge, what is the probability that an 
+    float   refugeAlienInflowProb;  ///< If this is a refuge, what is the probability that an
                                     ///< alien species attempting to germinate in this area will
                                     ///< do so successfully
     bool    seedOutflowAllowed;     ///< Can the plants germinate outside of the initial distrib area?
     bool    seedOutflowRestricted;  ///< If seedOutflowAllowed, is it restricted?
-    float   seedOutflowProb;        ///< If seedOutflowAllowed and seedOutflowRestricted, what is 
+    float   seedOutflowProb;        ///< If seedOutflowAllowed and seedOutflowRestricted, what is
                                     ///<    the probability that they are successful if attempting
                                     ///<    to do so?
     bool    reproLocalDensityConstrained;   ///< During reproduction, is the maximum density of
