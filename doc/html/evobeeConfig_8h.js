807ffeca395559c12aa7a35db5ee28bdf82f213e
Tim Taylor <tim@tim-taylor.com>
2017-08-17 16:33:37 +1000

    Updated docs

--- doc/html/evobeeConfig_8h.js
+++ doc/html/evobeeConfig_8h.js
@@ -1,5 +1,7 @@
 var evobeeConfig_8h =
 [
+    [ "evobee_GIT_BRANCH", "evobeeConfig_8h.html#a02163387a51f1dc43ca2f4c060a63204", null ],
+    [ "evobee_GIT_COMMIT_HASH", "evobeeConfig_8h.html#abbfc4af07bc940ea5b7c2fba83a6963d", null ],
     [ "evobee_VERSION_MAJOR", "evobeeConfig_8h.html#a49d50bee94975e820b4780756a963477", null ],
     [ "evobee_VERSION_MINOR", "evobeeConfig_8h.html#a6af5614ca2101f759cf5bb5055d5fef7", null ],
     [ "evobee_VERSION_PATCH", "evobeeConfig_8h.html#af7bc3feb8cb066561cf4fef037df96a7", null ],
--- doc/html/search/functions_8.js
+++ doc/html/search/functions_8.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['logexptsetup',['logExptSetup',['../classLogger.html#a4e022e5d88331bb94e929aae7351c721',1,'Logger']]],
   ['logflowersfull',['logFlowersFull',['../classLogger.html#a41e9a9ecf359d20721fe14d92a125a30',1,'Logger']]],
   ['logsdlerror',['logSDLError',['../sdl__tools_8h.html#a343bdc6b0f085211d4043f2d9485476c',1,'sdl_tools.h']]],
   ['losepollentoair',['losePollenToAir',['../classPollinator.html#aedfce6be503ca4094ae7590a523cdb53',1,'Pollinator']]]
