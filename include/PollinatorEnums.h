30735d8a6e49d0add99ba98fb9815b5c0dca9a79
Tim Taylor <tim@tim-taylor.com>
2017-09-06 18:40:56 +1000

    Changed file name and added definition of PollinatorForagingStrategy in file

---
+++ include/PollinatorEnums.h
@@ -0,0 +1,20 @@
+/**
+ * @file
+ *
+ * Declaration of enums associated with Pollinators: PollinatorConstancyType and PollinatorForagingStrategy
+ */
+
+#ifndef _POLLINATORENUMS_H
+#define _POLLINATORENUMS_H
+
+/**
+ * The PollinatorConstancyType enum
+ */
+ enum class PollinatorConstancyType {NONE, SIMPLE};
+
+/**
+ * The PollinatorConstancyType enum
+ */
+ enum class PollinatorForagingStrategy {RANDOM, NEAREST_FLOWER};
+
+#endif /* _POLLINATORENUMS_H */
