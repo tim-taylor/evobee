6f7b453d8ff23718e4ea2af2c08f28d06a3541e7
Tim Taylor <tim@tim-taylor.com>
2017-12-11 13:03:44 +0000

    Initial (delayed) check-in of this script to plot results of env size expts

---
+++ utils/plot-results-envsize.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+#
+# Script to plot evobee results from multiple runs with error bars
+#
+# Usage: plot-results.py title statsfile [statsfile2 [statsfile3 ...]]
+#   where each statsfile is a CSV format with layout: SZ,N,min,Q1,median,Q3,max,mean,sd
+#    C=constancy, P=patchiness, and the rest is the output of the command:
+#    sta --min --max --mean --sd --sample --q --delimiter $',' < pollination-50pc-times-s$S-c0p$C.txt
+#
+# Outputs: a graphics file called graph-<title>.pdf
+#
+# Author: Tim Taylor (http://www.tim-taylor.com)
+# Last update: 9 October 2017
+
+import sys
+import os
+import csv
+import re
+import matplotlib.pyplot as plt
+
+
+def main():
+
+    # check we have all of the required command line info
+    if len(sys.argv) < 3:
+        print("Usage: {} title statsfile [statsfile2 [statsfile3 ...]]"
+            .format(os.path.basename(sys.argv[0])), file=sys.stderr)
+        sys.exit(1)
+
+    # parse the command line info
+    title = sys.argv[1]
+    statsfilenames = [sys.argv[i] for i in range(2, len(sys.argv))]
+
+    for sfile in statsfilenames:
+
+        if not os.path.exists(sfile) or not os.path.isfile(sfile):
+            print("Stats file '{}' does not exist or is not a regular file!".format(sfile), file=sys.stderr)
+            exit(1)
+
+        with open(sfile, 'r') as f:
+            reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
+            data = list(reader)
+
+        x = [row[0] for row in data] # EnvSize
+        y = [row[4] for row in data] # Median
+        #y = [row[7] for row in data] # Mean
+        e = [row[8] for row in data] # StdDev
+
+        xmin = x[0]
+        xmax = x[-1]
+
+        m = re.match(r".*-(.*).txt", sfile)
+        desc = m.group(1)
+
+        plt.errorbar(x, y, yerr=e, fmt='-o', label=desc)
+
+
+    plt.xlabel('Environment size')
+    plt.ylabel('Fixation time')
+    plt.legend(loc='upper left', prop={'size': 10})
+    plt.title(title)
+    plt.grid()
+    plt.xlim(xmin-2, xmax+2)
+    #plt.ylim(140,350)
+    #plt.ylim(140,550)
+    #plt.show()
+
+    # Replace spaces etc in title so we can use it in the filename of the graph
+    filename = 'graph-'+title+'.pdf'
+    for ch in [' ',',','(',')','[',']']:
+        if ch in filename:
+            filename = filename.replace(ch,"-")
+    filename = filename.replace('---','-')
+    filename = filename.replace('--','-')
+
+
+    plt.savefig(filename)
+
+
+
+##-------------------------------------------------------##
+
+if __name__ == '__main__':
+    main()
