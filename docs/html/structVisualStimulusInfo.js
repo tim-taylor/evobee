39505c4443e03058314eaab9566ab70133f8d02e
Tim Taylor <tim@tim-taylor.com>
2019-09-17 15:43:02 +0100

    Updated documentation

---
+++ docs/html/structVisualStimulusInfo.js
@@ -0,0 +1,10 @@
+var structVisualStimulusInfo =
+[
+    [ "VisualStimulusInfo", "structVisualStimulusInfo.html#a1c3f7a1775f889aa3377463ae255c7d7", null ],
+    [ "VisualStimulusInfo", "structVisualStimulusInfo.html#a91a30da019aa03f03f8fb01258c64325", null ],
+    [ "detectionProb", "structVisualStimulusInfo.html#ad7113ed69e0dc7ed3c7a22ee90012f99", null ],
+    [ "greenContrast", "structVisualStimulusInfo.html#ad663574c2d7875a0c53a2bb2b073c948", null ],
+    [ "mp", "structVisualStimulusInfo.html#af383e56202e61e0458eaa4d193b63db4", null ],
+    [ "x", "structVisualStimulusInfo.html#a08b4044a45bd0c5383bf7c742d65734a", null ],
+    [ "y", "structVisualStimulusInfo.html#ab40dc401621ebc635cb8edb40897b756", null ]
+];
