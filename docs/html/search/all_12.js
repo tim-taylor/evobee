5c962ff9d1cde49d5b4fd9bf710357efb05ed31b
Tim Taylor <tim@tim-taylor.com>
2019-11-04 18:03:11 +0000

    Added new doc files

---
+++ docs/html/search/all_12.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['visitflower',['visitFlower',['../classPollinator.html#a348a3186b001ded8251f44bd5ebc5af5',1,'Pollinator']]],
+  ['visualiser',['Visualiser',['../classVisualiser.html',1,'']]],
+  ['visualiser_2ecpp',['Visualiser.cpp',['../Visualiser_8cpp.html',1,'']]],
+  ['visualiser_2eh',['Visualiser.h',['../Visualiser_8h.html',1,'']]],
+  ['visualpreferenceinfo',['VisualPreferenceInfo',['../structVisualPreferenceInfo.html',1,'']]],
+  ['visualstimulusinfo',['VisualStimulusInfo',['../structVisualStimulusInfo.html',1,'']]]
+];
---
+++ docs/html/structVisualPreferenceInfo.js
@@ -0,0 +1,22 @@
+var structVisualPreferenceInfo =
+[
+    [ "VisualPreferenceInfo", "structVisualPreferenceInfo.html#a46a5acd716df76caf2e68208f83d8122", null ],
+    [ "VisualPreferenceInfo", "structVisualPreferenceInfo.html#ab14211d8d5e49107c6ef7cb0171a6eb6", null ],
+    [ "constrainProbLandNonTarget", "structVisualPreferenceInfo.html#a378ef4dd6d70c6a94c1fc380015e4538", null ],
+    [ "constrainProbLandTarget", "structVisualPreferenceInfo.html#a432dadf4648c3539b2b6ca18f2f674b4", null ],
+    [ "decrementProbLandNonTarget", "structVisualPreferenceInfo.html#ae7ed7c8966f9266e0b5ec91dd91d8121", null ],
+    [ "decrementProbLandTarget", "structVisualPreferenceInfo.html#a5320c9fea033595d18a56395865e8d01", null ],
+    [ "getMarkerPoint", "structVisualPreferenceInfo.html#ad8abc71e433beb76e17e6ff98f6ec8aa", null ],
+    [ "getProbLandNonTarget", "structVisualPreferenceInfo.html#a2127107f44f0c7ddd80ffdb2d596f7f7", null ],
+    [ "getProbLandTarget", "structVisualPreferenceInfo.html#ac1df40573a95b65854f8a0b2c464d6ea", null ],
+    [ "incrementProbLandNonTarget", "structVisualPreferenceInfo.html#a4e0b548a374b11d631c098b713521a97", null ],
+    [ "incrementProbLandTarget", "structVisualPreferenceInfo.html#abc95b45b63a9955b3aff9f3c49a8cbfa", null ],
+    [ "reset", "structVisualPreferenceInfo.html#ab1737ecc62709865e0530ac94ebeb7ac", null ],
+    [ "setNonTarget", "structVisualPreferenceInfo.html#a469f9254c37707371dcec40b124c0ca3", null ],
+    [ "setTarget", "structVisualPreferenceInfo.html#a350e27c2e7f624c110403888052c6d37", null ],
+    [ "baseProbLandNonTarget", "structVisualPreferenceInfo.html#ab228a2dc3607ccb03f034c74ff3083b2", null ],
+    [ "baseProbLandTarget", "structVisualPreferenceInfo.html#a070ef2cf239751bb61ab76a8153c5d26", null ],
+    [ "mp", "structVisualPreferenceInfo.html#a487d740a144b30aec6088676139181aa", null ],
+    [ "probLandNonTarget", "structVisualPreferenceInfo.html#a7fc72f4b8de18ce9cc42b51c416a3e6a", null ],
+    [ "probLandTarget", "structVisualPreferenceInfo.html#a12916e926f44b6d2bdfc6b637368c260", null ]
+];
